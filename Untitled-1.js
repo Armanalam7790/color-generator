403f75b31c7f46b728e2d219751b3b47a8a59da1
Armanalam7790 <alamarman7790@gmail.com>
2025-11-14 22:56:38 +0530

    changed

---
+++ Untitled-1.js
@@ -0,0 +1,33 @@
+// var a = Math.random(); //it is method of Math object to generate random number between 0 to 1
+
+// var a = Math.random() * 100
+
+// var b  =  Math .floor(a)
+// console.log(b);
+
+// var arr  = [10,20,30,40,43,34,5,6,78,88,3,4,54,23,45,67,89,90,100,23,45,67,78,89,90]
+
+// console.log(arr.length-1);
+
+// var arr  = ["vedanr","abhi", "sovik", "sumit","ankit", "satwik","sarthak","ritik","harsh"]
+
+// var a  = Math.floor(Math.random()* arr.length)
+// console.log(arr[a]);
+
+// var arr  = ["vedanr","abhi", "sovik", "sumit","ankit", "satwik","sarthak","ritik","harsh"]
+
+// var btn  = document.querySelector("button")
+
+// btn.addEventListener("click", function () {
+//     var a  = Math.floor(Math.random()* arr.length)
+//     console.log(arr[a]);
+// })
+
+// var arr =  [
+//     {team:"csk",
+//      primary:"yellow",
+
+//     }: ,
+//     {},
+//     {,}
+// ]
